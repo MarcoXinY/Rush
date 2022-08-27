a607b085564ad581c02917b62aaed784a6a64436
chiyuan123 <1241846683@qq.com>
2022-08-27 17:24:26 +0800

    add .gitkeep add  simple Service code

---
+++ src/Client/code/Client.cpp
@@ -0,0 +1,3 @@
+//client 是在windows 上运行的 所以client code
+//在windows 上开发(运行在windows上)
+//所以目前linux 上没有 client 的代码
--- src/Service/code/Server.cpp
+++ src/Service/code/Server.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define BUFSIZE 50
+ 
+const int PORT = 990;
+const char *IPAddress = "172.27.15.149";
 
-int main()
-{
-	return 0;
+int main(){
+    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+ 
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(IPAddress);
+    serv_addr.sin_port = htons(990);
+    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+ 
+    listen(serv_sock, 20);
+ 
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    int clnt_sock = -1;
+    do {
+         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+         if (clnt_sock < 0) {
+             printf("Connected Error, re-try to get connected with client ... \n");
+         }
+    } while(clnt_sock < 0);
+    while(1) {
+        char strBuffer[BUFSIZE];
+        int dataLength = read(clnt_sock, strBuffer, sizeof(strBuffer)-1);
+        if (dataLength < 0) {
+            printf("Read Error ... \n");
+            continue;
+        }
+        printf("The server has already got the data: %s\n", strBuffer);
+ 
+ 
+        int writeFlag = write(clnt_sock, strBuffer, dataLength);
+        if (writeFlag < 0) {
+            printf("Write error ... \n");
+        }
+        printf("Has already sent the data to the client");
+ 
+    }
+    close(clnt_sock);
+    close(serv_sock);
+ 
+    return 0;
 }
